fcc779f6ce6aeedd9a27db8901fc2f2f1fd19f3c
muratbolu <muratbolu2000@gmail.com>
2023-01-02 16:42:27 +0300

    Try completely unrolling
    Does not work

--- kernels.c
+++ kernels.c
@@ -84,6 +84,295 @@ void convolution(int dim, pixel *src, pixel *ker, unsigned *dst)
     }
 }
 
+void convolution_completely_unrolled_doesnt_work(int dim, pixel *src, pixel *ker, unsigned *dst)
+{
+
+    unsigned i,sum=0;
+
+    for(i = 0; i < dim-7; i++) {
+
+    sum += src[i].red   * ker[0].red;
+    sum += src[i].green * ker[0].green;
+    sum += src[i].blue  * ker[0].blue;
+
+    sum += src[i+1].red   * ker[1].red;
+    sum += src[i+1].green * ker[1].green;
+    sum += src[i+1].blue  * ker[1].blue;
+
+    sum += src[i+2].red   * ker[2].red;
+    sum += src[i+2].green * ker[2].green;
+    sum += src[i+2].blue  * ker[2].blue;
+
+    sum += src[i+3].red   * ker[3].red;
+    sum += src[i+3].green * ker[3].green;
+    sum += src[i+3].blue  * ker[3].blue;
+
+    sum += src[i+4].red   * ker[4].red;
+    sum += src[i+4].green * ker[4].green;
+    sum += src[i+4].blue  * ker[4].blue;
+
+    sum += src[i+5].red   * ker[5].red;
+    sum += src[i+5].green * ker[5].green;
+    sum += src[i+5].blue  * ker[5].blue;
+
+    sum += src[i+6].red   * ker[6].red;
+    sum += src[i+6].green * ker[6].green;
+    sum += src[i+6].blue  * ker[6].blue;
+
+    sum += src[i+7].red   * ker[7].red;
+    sum += src[i+7].green * ker[7].green;
+    sum += src[i+7].blue  * ker[7].blue;
+
+    dst[i] = sum;
+    sum = 0;
+
+    sum += src[i+dim].red   * ker[8].red;
+    sum += src[i+dim].green * ker[8].green;
+    sum += src[i+dim].blue  * ker[8].blue;
+
+    sum += src[i+dim+1].red   * ker[9].red;
+    sum += src[i+dim+1].green * ker[9].green;
+    sum += src[i+dim+1].blue  * ker[9].blue;
+
+    sum += src[i+dim+2].red   * ker[10].red;
+    sum += src[i+dim+2].green * ker[10].green;
+    sum += src[i+dim+2].blue  * ker[10].blue;
+
+    sum += src[i+dim+3].red   * ker[11].red;
+    sum += src[i+dim+3].green * ker[11].green;
+    sum += src[i+dim+3].blue  * ker[11].blue;
+
+    sum += src[i+dim+4].red   * ker[12].red;
+    sum += src[i+dim+4].green * ker[12].green;
+    sum += src[i+dim+4].blue  * ker[12].blue;
+
+    sum += src[i+dim+5].red   * ker[13].red;
+    sum += src[i+dim+5].green * ker[13].green;
+    sum += src[i+dim+5].blue  * ker[13].blue;
+
+    sum += src[i+dim+6].red   * ker[14].red;
+    sum += src[i+dim+6].green * ker[14].green;
+    sum += src[i+dim+6].blue  * ker[14].blue;
+
+    sum += src[i+dim+7].red   * ker[15].red;
+    sum += src[i+dim+7].green * ker[15].green;
+    sum += src[i+dim+7].blue  * ker[15].blue;
+
+    dst[i+dim] = sum;
+    sum = 0;
+
+    sum += src[i+2*dim].red   * ker[16].red;
+    sum += src[i+2*dim].green * ker[16].green;
+    sum += src[i+2*dim].blue  * ker[16].blue;
+
+    sum += src[i+2*dim+1].red   * ker[17].red;
+    sum += src[i+2*dim+1].green * ker[17].green;
+    sum += src[i+2*dim+1].blue  * ker[17].blue;
+
+    sum += src[i+2*dim+2].red   * ker[18].red;
+    sum += src[i+2*dim+2].green * ker[18].green;
+    sum += src[i+2*dim+2].blue  * ker[18].blue;
+
+    sum += src[i+2*dim+3].red   * ker[19].red;
+    sum += src[i+2*dim+3].green * ker[19].green;
+    sum += src[i+2*dim+3].blue  * ker[19].blue;
+
+    sum += src[i+2*dim+4].red   * ker[20].red;
+    sum += src[i+2*dim+4].green * ker[20].green;
+    sum += src[i+2*dim+4].blue  * ker[20].blue;
+
+    sum += src[i+2*dim+5].red   * ker[21].red;
+    sum += src[i+2*dim+5].green * ker[21].green;
+    sum += src[i+2*dim+5].blue  * ker[21].blue;
+
+    sum += src[i+2*dim+6].red   * ker[22].red;
+    sum += src[i+2*dim+6].green * ker[22].green;
+    sum += src[i+2*dim+6].blue  * ker[22].blue;
+
+    sum += src[i+2*dim+7].red   * ker[23].red;
+    sum += src[i+2*dim+7].green * ker[23].green;
+    sum += src[i+2*dim+7].blue  * ker[23].blue;
+
+    dst[i+2*dim] = sum;
+    sum = 0;
+
+    sum += src[i+3*dim].red   * ker[24].red;
+    sum += src[i+3*dim].green * ker[24].green;
+    sum += src[i+3*dim].blue  * ker[24].blue;
+
+    sum += src[i+3*dim+1].red   * ker[25].red;
+    sum += src[i+3*dim+1].green * ker[25].green;
+    sum += src[i+3*dim+1].blue  * ker[25].blue;
+
+    sum += src[i+3*dim+2].red   * ker[26].red;
+    sum += src[i+3*dim+2].green * ker[26].green;
+    sum += src[i+3*dim+2].blue  * ker[26].blue;
+
+    sum += src[i+3*dim+3].red   * ker[27].red;
+    sum += src[i+3*dim+3].green * ker[27].green;
+    sum += src[i+3*dim+3].blue  * ker[27].blue;
+
+    sum += src[i+3*dim+4].red   * ker[28].red;
+    sum += src[i+3*dim+4].green * ker[28].green;
+    sum += src[i+3*dim+4].blue  * ker[28].blue;
+
+    sum += src[i+3*dim+5].red   * ker[29].red;
+    sum += src[i+3*dim+5].green * ker[29].green;
+    sum += src[i+3*dim+5].blue  * ker[29].blue;
+
+    sum += src[i+3*dim+6].red   * ker[30].red;
+    sum += src[i+3*dim+6].green * ker[30].green;
+    sum += src[i+3*dim+6].blue  * ker[30].blue;
+
+    sum += src[i+3*dim+7].red   * ker[31].red;
+    sum += src[i+3*dim+7].green * ker[31].green;
+    sum += src[i+3*dim+7].blue  * ker[31].blue;
+
+    dst[i+3*dim] = sum;
+    sum = 0;
+
+    sum += src[i+4*dim].red   * ker[32].red;
+    sum += src[i+4*dim].green * ker[32].green;
+    sum += src[i+4*dim].blue  * ker[32].blue;
+
+    sum += src[i+4*dim+1].red   * ker[33].red;
+    sum += src[i+4*dim+1].green * ker[33].green;
+    sum += src[i+4*dim+1].blue  * ker[33].blue;
+
+    sum += src[i+4*dim+2].red   * ker[34].red;
+    sum += src[i+4*dim+2].green * ker[34].green;
+    sum += src[i+4*dim+2].blue  * ker[34].blue;
+
+    sum += src[i+4*dim+3].red   * ker[35].red;
+    sum += src[i+4*dim+3].green * ker[35].green;
+    sum += src[i+4*dim+3].blue  * ker[35].blue;
+
+    sum += src[i+4*dim+4].red   * ker[36].red;
+    sum += src[i+4*dim+4].green * ker[36].green;
+    sum += src[i+4*dim+4].blue  * ker[36].blue;
+
+    sum += src[i+4*dim+5].red   * ker[37].red;
+    sum += src[i+4*dim+5].green * ker[37].green;
+    sum += src[i+4*dim+5].blue  * ker[37].blue;
+
+    sum += src[i+4*dim+6].red   * ker[38].red;
+    sum += src[i+4*dim+6].green * ker[38].green;
+    sum += src[i+4*dim+6].blue  * ker[38].blue;
+
+    sum += src[i+4*dim+7].red   * ker[39].red;
+    sum += src[i+4*dim+7].green * ker[39].green;
+    sum += src[i+4*dim+7].blue  * ker[39].blue;
+
+    dst[i+4*dim] = sum;
+    sum = 0;
+
+    sum += src[i+5*dim].red   * ker[40].red;
+    sum += src[i+5*dim].green * ker[40].green;
+    sum += src[i+5*dim].blue  * ker[40].blue;
+
+    sum += src[i+5*dim+1].red   * ker[41].red;
+    sum += src[i+5*dim+1].green * ker[41].green;
+    sum += src[i+5*dim+1].blue  * ker[41].blue;
+
+    sum += src[i+5*dim+2].red   * ker[42].red;
+    sum += src[i+5*dim+2].green * ker[42].green;
+    sum += src[i+5*dim+2].blue  * ker[42].blue;
+
+    sum += src[i+5*dim+3].red   * ker[43].red;
+    sum += src[i+5*dim+3].green * ker[43].green;
+    sum += src[i+5*dim+3].blue  * ker[43].blue;
+
+    sum += src[i+5*dim+4].red   * ker[44].red;
+    sum += src[i+5*dim+4].green * ker[44].green;
+    sum += src[i+5*dim+4].blue  * ker[44].blue;
+
+    sum += src[i+5*dim+5].red   * ker[45].red;
+    sum += src[i+5*dim+5].green * ker[45].green;
+    sum += src[i+5*dim+5].blue  * ker[45].blue;
+
+    sum += src[i+5*dim+6].red   * ker[46].red;
+    sum += src[i+5*dim+6].green * ker[46].green;
+    sum += src[i+5*dim+6].blue  * ker[46].blue;
+
+    sum += src[i+5*dim+7].red   * ker[47].red;
+    sum += src[i+5*dim+7].green * ker[47].green;
+    sum += src[i+5*dim+7].blue  * ker[47].blue;
+
+    dst[i+5*dim] = sum;
+    sum = 0;
+
+    sum += src[i+6*dim].red   * ker[48].red;
+    sum += src[i+6*dim].green * ker[48].green;
+    sum += src[i+6*dim].blue  * ker[48].blue;
+
+    sum += src[i+6*dim+1].red   * ker[49].red;
+    sum += src[i+6*dim+1].green * ker[49].green;
+    sum += src[i+6*dim+1].blue  * ker[49].blue;
+
+    sum += src[i+6*dim+2].red   * ker[50].red;
+    sum += src[i+6*dim+2].green * ker[50].green;
+    sum += src[i+6*dim+2].blue  * ker[50].blue;
+
+    sum += src[i+6*dim+3].red   * ker[51].red;
+    sum += src[i+6*dim+3].green * ker[51].green;
+    sum += src[i+6*dim+3].blue  * ker[51].blue;
+
+    sum += src[i+6*dim+4].red   * ker[52].red;
+    sum += src[i+6*dim+4].green * ker[52].green;
+    sum += src[i+6*dim+4].blue  * ker[52].blue;
+
+    sum += src[i+6*dim+5].red   * ker[53].red;
+    sum += src[i+6*dim+5].green * ker[53].green;
+    sum += src[i+6*dim+5].blue  * ker[53].blue;
+
+    sum += src[i+6*dim+6].red   * ker[54].red;
+    sum += src[i+6*dim+6].green * ker[54].green;
+    sum += src[i+6*dim+6].blue  * ker[54].blue;
+
+    sum += src[i+6*dim+7].red   * ker[55].red;
+    sum += src[i+6*dim+7].green * ker[55].green;
+    sum += src[i+6*dim+7].blue  * ker[55].blue;
+
+    dst[i+6*dim] = sum;
+    sum = 0;
+
+    sum += src[i+7*dim].red   * ker[56].red;
+    sum += src[i+7*dim].green * ker[56].green;
+    sum += src[i+7*dim].blue  * ker[56].blue;
+
+    sum += src[i+7*dim+1].red   * ker[57].red;
+    sum += src[i+7*dim+1].green * ker[57].green;
+    sum += src[i+7*dim+1].blue  * ker[57].blue;
+
+    sum += src[i+7*dim+2].red   * ker[58].red;
+    sum += src[i+7*dim+2].green * ker[58].green;
+    sum += src[i+7*dim+2].blue  * ker[58].blue;
+
+    sum += src[i+7*dim+3].red   * ker[59].red;
+    sum += src[i+7*dim+3].green * ker[59].green;
+    sum += src[i+7*dim+3].blue  * ker[59].blue;
+
+    sum += src[i+7*dim+4].red   * ker[60].red;
+    sum += src[i+7*dim+4].green * ker[60].green;
+    sum += src[i+7*dim+4].blue  * ker[60].blue;
+
+    sum += src[i+7*dim+5].red   * ker[61].red;
+    sum += src[i+7*dim+5].green * ker[61].green;
+    sum += src[i+7*dim+5].blue  * ker[61].blue;
+
+    sum += src[i+7*dim+6].red   * ker[62].red;
+    sum += src[i+7*dim+6].green * ker[62].green;
+    sum += src[i+7*dim+6].blue  * ker[62].blue;
+
+    sum += src[i+7*dim+7].red   * ker[63].red;
+    sum += src[i+7*dim+7].green * ker[63].green;
+    sum += src[i+7*dim+7].blue  * ker[63].blue;
+
+    dst[i+7*dim] = sum;
+    sum = 0;
+    }
+}
+
 void convolution_using_stack(int dim, pixel *src, pixel *ker, unsigned *dst)
 {
 
@@ -226,7 +515,7 @@ void average_pooling(int dim, pixel *src, pixel *dst)
  *     of the average pooling  with the driver by calling the
  *     add_average_pooling_function() for each test function. When you run the
  *     driver program, it will test and report the performance of each
- *     registered test function.  
+ *     registered test function.
  ******************************************************************************/
 
 void register_average_pooling_functions() {
